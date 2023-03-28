537cacef750baa5b984a4936bcb502395d3a5ced
MrHusnain <husnainmahoon@gmail.com>
2023-03-28 14:20:08 +0500

    item add and show all items api setup

--- src/main/java/com/example/Pract/Services/ItemServices.java
+++ src/main/java/com/example/Pract/Services/ItemServices.java
@@ -1,36 +1,44 @@
 package com.example.Pract.Services;
 
 import com.example.Pract.Entity.Item;
-import com.example.Pract.Model.itemModel;
+import com.example.Pract.Model.ItemModel;
 import com.example.Pract.Repository.ItemRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Service
+@Slf4j
 public class ItemServices {
     @Autowired
-ItemRepository itemRepository;
+    ItemRepository itemRepository;
+    public void CreateItem(ItemModel itemModel){
+    Item item = Item.builder()
+            .itemName(itemModel.getItemName())
+            .itemPrice(itemModel.getItemPrice())
+            .build();
+    itemRepository.save(item);
+    log.info("Product {} is saved ",item.getItemId());
 
-    public List<itemModel> GetAllitemList(){
+                }
+
+
+    public List<ItemModel> GetAllitemList(){
         return itemRepository.findAll()
                 .stream()
                 .map(this::convertEntitytoModel)
                 .collect(Collectors.toList());
     }
 
-    private itemModel convertEntitytoModel(Item item){
-        itemModel itemModel=new itemModel();
+    private ItemModel convertEntitytoModel(Item item){
+        ItemModel itemModel=new ItemModel();
         itemModel.setItemName(item.getItemName());
         itemModel.setItemId(item.getItemId());
         itemModel.setItemPrice(item.getItemPrice());
